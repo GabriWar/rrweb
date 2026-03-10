43e8270cb75895c065f1fbc82b82795176b085c8
Billy Vong <billyvg@users.noreply.github.com>
2026-03-10 12:09:09 -0400

    build: fix craft pre-release  (#268)

--- scripts/craft-pre-release.sh
+++ scripts/craft-pre-release.sh
@@ -4,13 +4,15 @@ set -eux
 # Move to the project root
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $SCRIPT_DIR/..
-OLD_VERSION="${1}"
-NEW_VERSION="${2}"
+OLD_VERSION=${CRAFT_OLD_VERSION}
+NEW_VERSION=${CRAFT_NEW_VERSION}
+
 
 # Do not tag and commit changes made by "npm version"
 export npm_config_git_tag_version=false
 
 yarn install --frozen-lockfile
+
 # --force-publish - force publish all packages, this will skip the lerna changed check for changed packages and forces a package that didn't have a git diff change to be updated.
 # --exact - specify updated dependencies in updated packages exactly (with no punctuation), instead of as semver compatible (with a ^).
 # --no-git-tag-version - don't commit changes to package.json files and don't tag the release.
